4136791f2895a3cafcc111c3ba5c3958a6e6fb56
Coleronius <cokacolea06@gmail.com>
2025-03-01 16:20:47 -0500

    Finished menu layout

---
+++ UI/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_htp_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
